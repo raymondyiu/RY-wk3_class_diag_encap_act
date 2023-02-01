cc9b1f109b57c81f504bc825e3b2bfd4633cab48
Raymond <raymond.yiu@gmail.com>
2023-02-01 22:17:23 +0000

    try JUnit test

---
+++ src/test/java/org/example/StudentTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @org.junit.jupiter.api.Test
+    void upgrade() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void downgrade() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDetail() {
+    }
+}
